bfaab12a5a365e718fc70e115e919a47018e5abc
sandrakarlberg <sandra.karlberg@hotmail.com>
2026-01-28 15:03:37 +0100

    #31 feat: adds navigation buttons to GroupView

--- client/src/app/components/GroupHandling/GroupView.tsx
+++ client/src/app/components/GroupHandling/GroupView.tsx
@@ -3,6 +3,7 @@
 import GroupCard from "./GroupCard";
 import { useTranslations } from "next-intl";
 import { Group } from "@/app/interfaces/group";
+import { Button } from "@sk-web-gui/react";
 
 interface GroupCardProps {
   list: Group[];
@@ -14,10 +15,16 @@ const GroupView = ({ list = [] }: GroupCardProps) => {
   return (
     <div className="flex flex-col gap-14">
       <p className="text-center text-h4-md sm:text-xl">{t("sectionTitle")}</p>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        {list.map((item) => (
-          <GroupCard key={item.id} item={item} />
-        ))}
+      <div className="flex flex-col gap-14 pb-28">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          {list.map((item) => (
+            <GroupCard key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <Button variant="tertiary">{t("goBackButton")}</Button>
+        <Button>{t("addButton")}</Button>
       </div>
     </div>
   );
