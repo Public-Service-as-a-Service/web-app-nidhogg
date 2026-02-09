0f32aba3961cb32aac28a7976517d349ef33331e
sandrakarlberg <sandra.karlberg@hotmail.com>
2026-02-09 11:03:16 +0100

    #81 feat: adds delete button and updates group layout

--- client/src/app/components/GroupHandling/MemberSection.tsx
+++ client/src/app/components/GroupHandling/MemberSection.tsx
@@ -1,29 +1,16 @@
 "use client";
 
 import MemberCard from "./MemberCard";
-import { Button } from "@sk-web-gui/react";
-import { useRouter } from "next/navigation";
 import { useTranslations } from "next-intl";
-import { SquarePen, ArrowLeft, Save } from "lucide-react";
 import { Employee } from "@/app/interfaces/employee";
 
 interface MemberSectionProps {
   members: Employee[];
   editMode: boolean;
-  handleEditMode: () => void;
 }
 
-const MemberSection = ({
-  members = [],
-  editMode,
-  handleEditMode,
-}: MemberSectionProps) => {
+const MemberSection = ({ members = [], editMode }: MemberSectionProps) => {
   const t = useTranslations("GroupHandling");
-  const router = useRouter();
-
-  const handleGoBack = () => {
-    router.back();
-  };
 
   return (
     <div className="flex flex-col pt-16">
@@ -32,23 +19,6 @@ const MemberSection = ({
         {members.map((member) => (
           <MemberCard key={member.id} member={member} editMode={editMode} />
         ))}
-      </div>
-      <div className="flex justify-between">
-        <Button variant="secondary" rounded={true} onClick={handleGoBack}>
-          <ArrowLeft />
-          {t("goBackButton")}
-        </Button>
-        {!editMode ? (
-          <Button onClick={handleEditMode}>
-            {t("editButton")}
-            <SquarePen />
-          </Button>
-        ) : (
-          <Button onClick={handleEditMode}>
-            {t("saveButton")}
-            <Save />
-          </Button>
-        )}
       </div>
     </div>
   );
--- client/src/app/dashboard/groups/[id]/page.tsx
+++ client/src/app/dashboard/groups/[id]/page.tsx
@@ -4,9 +4,12 @@ import { useGroup } from "@/app/services/useGroup";
 import Loading from "@/app/components/LoadingSpinner";
 import { useTranslations } from "next-intl";
 import MainWrapper from "@/app/components/MainWrapper";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import MemberSection from "@/app/components/GroupHandling/MemberSection";
 import { useState } from "react";
+import dayjs from "dayjs";
+import { Button } from "@sk-web-gui/react";
+import { ArrowLeft, SquarePen, Save, Trash2 } from "lucide-react";
 
 const EditGroup = () => {
   const [isEditing, setIsEditing] = useState(false);
@@ -14,6 +17,7 @@ const EditGroup = () => {
   const params = useParams();
   const id = Number(params.id);
   const t = useTranslations("GroupHandling");
+  const router = useRouter();
 
   const { data: group, isLoading } = useGroup(id);
 
@@ -36,14 +40,47 @@ const EditGroup = () => {
   return (
     <MainWrapper>
       <div className="pt-44 px-20 w-[330px] sm:w-[450px] md:w-[700px]">
-        <h1 className="text-h2-sm">{group.name}</h1>
-        <p className="text-label-large">{t("groupDescription")}</p>
-        <p>{group.description}</p>
-        <MemberSection
-          members={group.employees}
-          editMode={isEditing}
-          handleEditMode={handleEditMode}
-        />
+        <div className="flex justify-between pb-40">
+          <Button variant="secondary">
+            {t("deleteButton")}
+            <Trash2 />
+          </Button>
+          {!isEditing ? (
+            <Button onClick={handleEditMode}>
+              {t("editButton")}
+              <SquarePen />
+            </Button>
+          ) : (
+            <Button onClick={handleEditMode}>
+              {t("saveButton")}
+              <Save />
+            </Button>
+          )}
+        </div>
+        <div className="flex flex-col gap-16">
+          <div className="flex flex-col gap-10">
+            <h1 className="text-h2-sm !m-0">{group.name}</h1>
+            <p className="text-small">
+              {t("wasCreated")}
+              {dayjs(group.createdAt).format("YYYY-MM-DD")}
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-label-large !m-0">{t("groupDescription")}</p>
+            <p>{group.description}</p>
+          </div>
+        </div>
+        <MemberSection members={group.employees} editMode={isEditing} />
+        <div className="flex justify-start">
+          <Button
+            variant="secondary"
+            rounded={true}
+            onClick={() => router.back()}
+          >
+            <ArrowLeft />
+            {t("goBackButton")}
+          </Button>
+        </div>
       </div>
     </MainWrapper>
   );
