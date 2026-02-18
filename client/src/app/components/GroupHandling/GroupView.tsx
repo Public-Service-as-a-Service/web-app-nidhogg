11f105ca30bbc61a4aed56fc17a74a2f386563a8
sandrakarlberg <sandra.karlberg@hotmail.com>
2026-02-18 10:34:09 +0100

    #91 feat: adds navigation to create group page

--- client/src/app/components/GroupHandling/GroupView.tsx
+++ client/src/app/components/GroupHandling/GroupView.tsx
@@ -5,6 +5,7 @@ import { useTranslations } from "next-intl";
 import { Group } from "@/app/interfaces/group";
 import { Button } from "@sk-web-gui/react";
 import { UsersRound, ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface GroupCardProps {
   list: Group[];
@@ -12,11 +13,16 @@ interface GroupCardProps {
 
 const GroupView = ({ list = [] }: GroupCardProps) => {
   const t = useTranslations("GroupHandling");
+  const router = useRouter();
+
+  const handleCreate = () => {
+    router.push("/dashboard/groups/create");
+  };
 
   return (
     <div className="flex flex-col">
       <div className="flex justify-end pb-64">
-        <Button size="lg">
+        <Button size="lg" onClick={handleCreate}>
           {t("addButton")}
           <UsersRound />
         </Button>
