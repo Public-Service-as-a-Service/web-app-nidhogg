fb1578024453ddc1d89c79e4447a1f3dc7d47bda
sandrakarlberg <sandra.karlberg@hotmail.com>
2026-03-04 16:31:58 +0100

    #105 fix: updates delete group service route

--- client/src/app/services/useDeleteGroup.ts
+++ client/src/app/services/useDeleteGroup.ts
@@ -1,13 +1,13 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { Group } from "../interfaces/group";
-import { ROUTES } from "../constants";
+import { API_ENDPOINTS } from "../constants";
 
 export const useDeleteGroup = () => {
   return useMutation({
     mutationFn: async (groupId: number) => {
       const response = await axios.delete<Group>(
-        `${process.env.NEXT_PUBLIC_API_URL}${ROUTES.groups}/${groupId}`,
+        `${process.env.NEXT_PUBLIC_API_URL}${API_ENDPOINTS.groups}/${groupId}`,
         { withCredentials: true },
       );
       return response.data;
