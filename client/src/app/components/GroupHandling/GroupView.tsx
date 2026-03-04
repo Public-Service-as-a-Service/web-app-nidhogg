3cd3ff5955c9e476a6a5c21f3ec19e88345965e4
sandrakarlberg <sandra.karlberg@hotmail.com>
2026-03-04 13:14:23 +0100

    #105 feat: sorts groups from newest to oldest

--- client/src/app/components/GroupHandling/GroupView.tsx
+++ client/src/app/components/GroupHandling/GroupView.tsx
@@ -10,22 +10,21 @@ import { useState } from "react";
 import ShowAllToggleButton from "../ShowAllToggleButton";
 
 interface GroupCardProps {
-  list: Group[];
+  groups: Group[];
 }
 
-const GroupView = ({ list = [] }: GroupCardProps) => {
+const GroupView = ({ groups = [] }: GroupCardProps) => {
   const [amount, setAmount] = useState<number>(3);
   const t = useTranslations("GroupHandling");
   const router = useRouter();
 
-  const handleCreate = () => {
-    router.push("/dashboard/groups/create");
-  };
-
   return (
     <div className="flex flex-col">
       <div className="flex justify-end pb-64">
-        <Button size="lg" onClick={handleCreate}>
+        <Button
+          size="lg"
+          onClick={() => router.push("/dashboard/groups/create")}
+        >
           {t("createGroupButton")}
           <UsersRound />
         </Button>
@@ -33,13 +32,13 @@ const GroupView = ({ list = [] }: GroupCardProps) => {
       <h1 className="text-h2-sm">{t("sectionTitle")}</h1>
       <div className="flex flex-col gap-14 pb-16">
         <div className="flex flex-col gap-24">
-          {list.slice(0, amount).map((item) => (
+          {groups.slice(0, amount).map((item) => (
             <GroupCard key={item.id} item={item} />
           ))}
         </div>
       </div>
       <ShowAllToggleButton
-        totalCount={list.length}
+        totalCount={groups.length}
         visibleCount={amount}
         collapsedCount={3}
         onToggle={setAmount}
--- client/src/app/dashboard/groups/page.tsx
+++ client/src/app/dashboard/groups/page.tsx
@@ -4,15 +4,20 @@ import MainWrapper from "@/app/components/MainWrapper";
 import GroupView from "@/app/components/GroupHandling/GroupView";
 import Loading from "@/app/components/LoadingSpinner";
 import { useGroups } from "@/app/services/useGroups";
+import dayjs from "dayjs";
 
 const Groups = () => {
   const { data: groups = [], isLoading } = useGroups("user@test.se"); //byt ut till useUserEmail
+
+  const sortedGroups = [...groups].sort((a, b) =>
+    dayjs(b.createdAt).diff(dayjs(a.createdAt)),
+  );
 
   if (isLoading) return <Loading />;
 
   return (
     <MainWrapper>
-       <GroupView list={groups} />
+      <GroupView groups={sortedGroups} />
     </MainWrapper>
   );
 };
