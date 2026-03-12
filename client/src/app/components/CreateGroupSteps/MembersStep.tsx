3fee87ac9ce967fc9917af6aa76c20d6f987a574
sandrakarlberg <sandra.karlberg@hotmail.com>
2026-03-12 10:50:01 +0100

    #105 feat: implement placeholder hook for group creator id in MembersStep

--- client/src/app/components/CreateGroupSteps/MembersStep.tsx
+++ client/src/app/components/CreateGroupSteps/MembersStep.tsx
@@ -10,6 +10,7 @@ import { useCreateGroup } from "@/app/services/useCreateGroup";
 import { useRouter } from "next/navigation";
 import Loading from "../LoadingSpinner";
 import { PAGE_ROUTES } from "@/app/constants";
+import { useUserEmail } from "@/app/hooks/useUserEmail";
 
 interface MemberStepProps {
   onPrev: () => void;
@@ -29,6 +30,7 @@ const MembersStep = ({
   const t = useTranslations("GroupHandling");
   const router = useRouter();
   const mutation = useCreateGroup();
+  const email = useUserEmail();
 
   const memberIdSet = useMemo(
     () => new Set(members.map((member) => member.id)),
@@ -40,7 +42,7 @@ const MembersStep = ({
       {
         name: title,
         description: description,
-        creatorId: "user@test.se",
+        creatorId: email,
         employees: [...memberIdSet],
       },
       {
