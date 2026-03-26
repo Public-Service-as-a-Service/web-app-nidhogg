fcce4540d41be2248d44909d78d47b6e6e915d70
sandrakarlberg <sandra.karlberg@hotmail.com>
2026-03-26 16:24:34 +0100

    #180 feat: adds function to delete members during group creation

--- client/src/app/components/CreateGroupSteps/MembersStep.tsx
+++ client/src/app/components/CreateGroupSteps/MembersStep.tsx
@@ -16,6 +16,7 @@ interface MemberStepProps {
   onPrev: () => void;
   members: Employee[];
   handleBulkMembers: (recipientIds: Employee[]) => void;
+  handleDeleteMember: (id: number) => void;
   title: string;
   description: string;
 }
@@ -24,6 +25,7 @@ const MembersStep = ({
   onPrev,
   members,
   handleBulkMembers,
+  handleDeleteMember,
   title,
   description,
 }: MemberStepProps) => {
@@ -54,6 +56,8 @@ const MembersStep = ({
     );
   };
 
+  console.log(members);
+
   if (mutation.isPending) {
     return <Loading />;
   }
@@ -76,7 +80,7 @@ const MembersStep = ({
             <p className="self-center">
               {m.firstName} {m.lastName}
             </p>
-            <Button iconButton={true}>
+            <Button iconButton={true} onClick={() => handleDeleteMember(m.id)}>
               <Trash />
             </Button>
           </div>
