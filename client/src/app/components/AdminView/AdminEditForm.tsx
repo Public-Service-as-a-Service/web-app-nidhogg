32dca5e8af0d72cd7634876ffb31818405188add
Joakim Ring <joakim.ring@icloud.com>
2026-03-30 12:54:17 +0200

    import update

--- client/src/app/components/AdminView/AdminEditForm.tsx
+++ client/src/app/components/AdminView/AdminEditForm.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { useTranslations } from "next-intl";
 import { Button, Card, Input } from "@sk-web-gui/react";
+import { Trash, Save } from "lucide-react";
 import MainWrapper from "@/app/components/MainWrapper";
 import { type AdminUser, type UpdateUserRequest } from "@/app/services/useAdminUsers";
 
@@ -49,11 +50,6 @@ const tertiaryButtonProps = {
   style: { borderRadius: "12px", background: "rgba(28, 28, 40, 0.12)", border: "none" },
 };
 
-const TrashIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-    <path d="M13.5 5.25H4.5V15C4.5 15.1989 4.57907 15.3896 4.71973 15.5303C4.86038 15.6709 5.05109 15.75 5.25 15.75H12.75C12.9489 15.75 13.1396 15.6709 13.2803 15.5303C13.4209 15.3896 13.5 15.1989 13.5 15V5.25ZM11.25 3C11.25 2.80109 11.1709 2.61038 11.0303 2.46973C10.8896 2.32907 10.6989 2.25 10.5 2.25H7.5C7.30109 2.25 7.11038 2.32907 6.96973 2.46973C6.82907 2.61038 6.75 2.80109 6.75 3V3.75H11.25V3ZM12.75 3.75H15.75C16.1642 3.75 16.5 4.08579 16.5 4.5C16.5 4.91421 16.1642 5.25 15.75 5.25H15V15C15 15.5967 14.7628 16.1689 14.3408 16.5908C13.9189 17.0128 13.3467 17.25 12.75 17.25H5.25C4.65326 17.25 4.08114 17.0128 3.65918 16.5908C3.23722 16.1689 3 15.5967 3 15V5.25H2.25C1.83579 5.25 1.5 4.91421 1.5 4.5C1.5 4.08579 1.83579 3.75 2.25 3.75H5.25V3C5.25 2.40326 5.48722 1.83114 5.90918 1.40918C6.33114 0.987223 6.90326 0.75 7.5 0.75H10.5C11.0967 0.75 11.6689 0.987223 12.0908 1.40918C12.5128 1.83114 12.75 2.40326 12.75 3V3.75Z" fill="currentColor"/>
-  </svg>
-);
 
 const AdminEditForm = ({
   user,
@@ -93,7 +89,7 @@ const AdminEditForm = ({
                     <span style={labelMediumStyle}>{t("deleteCancelButton")}</span>
                   </Button>
                   <Button size="sm" onClick={onDelete} disabled={isDeleting}>
-                    <TrashIcon />
+                    <Trash size={18} />
                     <span style={labelMediumStyle}>{t("deleteButton")}</span>
                   </Button>
                 </div>
@@ -103,7 +99,7 @@ const AdminEditForm = ({
                     <span style={labelMediumStyle}>{isActive ? t("deactivateButton") : t("activateButton")}</span>
                   </Button>
                   <Button {...tertiaryButtonProps} onClick={() => setShowDeleteConfirm(true)}>
-                    <TrashIcon />
+                    <Trash size={18} />
                     <span style={labelMediumStyle}>{t("deleteButton")}</span>
                   </Button>
                 </div>
@@ -176,9 +172,7 @@ const AdminEditForm = ({
             <span style={{ fontSize: "18px", fontWeight: 700, lineHeight: "32px", fontVariantNumeric: "lining-nums proportional-nums" }}>
               {t("saveButton")}
             </span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
-              <path d="M5 10V15H11.6667V10H5ZM0 2.5C0 1.83696 0.263581 1.20126 0.732422 0.732422C1.20126 0.263581 1.83696 0 2.5 0H11.0116C11.668 0.00935033 12.2937 0.27752 12.7547 0.744629L12.7555 0.743815L15.9229 3.91113H15.922C16.3894 4.37217 16.6573 4.99846 16.6667 5.65511V14.1667C16.6667 14.8297 16.4031 15.4654 15.9342 15.9342C15.4654 16.4031 14.8297 16.6667 14.1667 16.6667H2.5C1.83696 16.6667 1.20126 16.4031 0.732422 15.9342C0.263581 15.4654 0 14.8297 0 14.1667V2.5ZM1.66667 14.1667C1.66667 14.3877 1.75453 14.5996 1.91081 14.7559C2.06709 14.9121 2.27899 15 2.5 15H3.33333V10C3.33333 9.55797 3.50905 9.13417 3.82161 8.82161C4.13417 8.50905 4.55797 8.33333 5 8.33333H11.6667C12.1087 8.33333 12.5325 8.50905 12.8451 8.82161C13.1576 9.13418 13.3333 9.55797 13.3333 10V15H14.1667C14.3877 15 14.5996 14.9121 14.7559 14.7559C14.9121 14.5996 15 14.3877 15 14.1667V5.67871L14.9951 5.59652C14.9737 5.4071 14.8875 5.22981 14.7502 5.09521L14.7437 5.08952V5.0887L11.5715 1.9165C11.4369 1.77918 11.2596 1.69301 11.0701 1.67155L10.988 1.66667H5V4.16667H10.8333C11.2936 4.16667 11.6667 4.53976 11.6667 5C11.6667 5.46024 11.2936 5.83333 10.8333 5.83333H5C4.55797 5.83333 4.13417 5.65761 3.82161 5.34505C3.50905 5.03249 3.33333 4.60869 3.33333 4.16667V1.66667H2.5C2.27899 1.66667 2.06709 1.75453 1.91081 1.91081C1.75453 2.06709 1.66667 2.27899 1.66667 2.5V14.1667Z" fill="currentColor"/>
-            </svg>
+            <Save size={17} />
           </Button>
         </div>
       </div>
--- client/src/app/components/AdminView/AdminUserList.tsx
+++ client/src/app/components/AdminView/AdminUserList.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { useTranslations } from "next-intl";
 import { Button, Card, Input } from "@sk-web-gui/react";
-import { Plus, Search } from "lucide-react";
+import { Plus, Search, SquarePen } from "lucide-react";
 import MainWrapper from "@/app/components/MainWrapper";
 import { type AdminUser } from "@/app/services/useAdminUsers";
 
@@ -96,9 +96,7 @@ const AdminUserList = ({ users, isLoading, isError, onEdit, onCreateClick }: Pro
               <div className="flex items-center justify-between w-full">
                 <span style={{ fontSize: "16px", fontWeight: 400, lineHeight: "24px", fontVariantNumeric: "lining-nums proportional-nums" }}>ID: {user.id}</span>
                 <Button size="sm" variant="secondary" className="dark:ring-[0.5px] dark:ring-white/5" style={{ borderRadius: "12px", background: "rgba(28, 28, 40, 0.12)", border: "none" }} onClick={() => onEdit(user)}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-                    <path d="M1.5 14.249V3.74902C1.50004 3.15235 1.73726 2.58012 2.15918 2.1582C2.58113 1.7363 3.1533 1.49902 3.75 1.49902H9C9.41416 1.49902 9.74991 1.83489 9.75 2.24902C9.75 2.66324 9.41421 2.99902 9 2.99902H3.75C3.55113 2.99902 3.36037 3.07815 3.21973 3.21875C3.07911 3.35936 3.00004 3.55017 3 3.74902V14.249C3 14.4479 3.07913 14.6387 3.21973 14.7793C3.36038 14.9199 3.55109 14.999 3.75 14.999H14.25C14.4489 14.999 14.6396 14.9199 14.7803 14.7793C14.9209 14.6387 15 14.4479 15 14.249V8.99902C15.0001 8.58489 15.3358 8.24902 15.75 8.24902C16.1642 8.24902 16.4999 8.58489 16.5 8.99902V14.249C16.5 14.8457 16.2627 15.4179 15.8408 15.8398C15.4189 16.2618 14.8467 16.499 14.25 16.499H3.75C3.15326 16.499 2.58114 16.2618 2.15918 15.8398C1.73728 15.4179 1.5 14.8457 1.5 14.249ZM15.7471 3.09277C15.747 2.86979 15.6587 2.65572 15.501 2.49805C15.3433 2.34038 15.1293 2.25195 14.9062 2.25195C14.6832 2.25195 14.4692 2.34038 14.3115 2.49805L7.552 9.25903H7.55127C7.4623 9.34785 7.39704 9.45776 7.36157 9.57837L6.92358 11.0747L8.42139 10.6375L8.51001 10.6052C8.56745 10.5803 8.62166 10.5484 8.67114 10.51L8.74146 10.4478L15.501 3.6875C15.6586 3.52979 15.7471 3.31578 15.7471 3.09277ZM17.2471 3.09277C17.2471 3.7136 17.0005 4.30903 16.5615 4.74805L9.802 11.509H9.80127C9.53417 11.7758 9.20419 11.9708 8.8418 12.0767L8.84253 12.0774L6.68774 12.7073H6.68701C6.49346 12.7637 6.28833 12.7668 6.09302 12.7168C5.89766 12.6667 5.71927 12.565 5.57666 12.4224C5.43418 12.2798 5.3323 12.1019 5.28223 11.9067C5.23218 11.7114 5.23528 11.5056 5.29175 11.312H5.29248L5.92236 9.15723V9.15649C6.01543 8.83952 6.17683 8.54702 6.39478 8.30029L6.49146 8.19775L13.251 1.4375C13.69 0.998532 14.2854 0.751953 14.9062 0.751953C15.5271 0.751953 16.1225 0.998532 16.5615 1.4375C17.0005 1.87648 17.247 2.47197 17.2471 3.09277Z" fill="currentColor"/>
-                  </svg>
+                  <SquarePen size={18} />
                   <span style={{ fontSize: "16px", fontWeight: 700, lineHeight: "24px", fontVariantNumeric: "lining-nums proportional-nums" }}>{t("editButton")}</span>
                 </Button>
               </div>
